6dc5f70774435d9c3acb890a85ef2063b79db9f2
Al-Saleh KEITA <alsaleh.keita@gmail.com>
2017-09-13 17:08:17 +0200

    modifs CollectorController

--- dev/src/Controller/CollectorController.php
+++ dev/src/Controller/CollectorController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Controller;
 
 use Entity\Collector;
@@ -15,37 +14,72 @@ class CollectorController extends ControllerAbstract{
             $collector
                 ->setLastname($_POST['lastname'])
                 ->setFirstname($_POST['firstname'])
-                ->setEmail($_POST['email']);
-
+                ->setPhonenumber($_POST['phone_number'])
+                ->setEmail($_POST['email'])
+                ->setStatus($_POST['status'])
+                ->setAddress($_POST['address'])
+                ->setCity($_POST['city'])
+                ->setPostalcode($_POST['postal_code']);
+            
             if(empty($_POST['lastname'])){
                 $errors['lastname'] = "Le nom est obligatoire";
-            }elseif(strlen($_POST['lastname'])>100){
-                $errors['lastname'] = "Le nom doit être inférieur à 100 caractères";
+            } elseif(strlen($_POST['lastname'])>45){
+                $errors['lastname'] = "Le nom doit être inférieur à 45 caractères";
             }
 
             if(empty($_POST['firstname'])){
                 $errors['firstname'] = "Le prénom est obligatoire";
-            }elseif(strlen($_POST['firstname'])>100){
-                $errors['firstname'] = "Le prénom doit être inférieur à 100 caractères";
+            } elseif(strlen($_POST['firstname'])>45){
+                $errors['firstname'] = "Le prénom doit être inférieur à 45 caractères";
+            }
+            
+            if(empty($_POST['phone_number'])){
+                $errors['phone_number'] = "Le numéro de téléphone est obligatoire";
+            } elseif(strlen($_POST['phone_number'])>45){
+                $errors['phone_number'] = "Le numéro de téléphone doit être inférieur à 45 caractères";
             }
 
             if(empty($_POST['email'])){
                 $errors['email'] = "L'email est obligatoire";
-            }elseif(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+            } elseif(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
                 $errors['email'] = "L'email n'est pas valide";
-            }elseif (!is_null($this->app['collector.repository']->findByEmail($_POST['email']))) {
+            } elseif (!is_null($this->app['collector.repository']->findByEmail($_POST['email']))) {
                 $errors['email'] = "L'email est déjà utilisé";
             }
+            
+            if(empty($_POST['status'])){
+                $errors['status'] = "Le statut (salarié/admin) est obligatoire";
+            } elseif(strlen($_POST['status'])>45){
+                $errors['status'] = "Le numéro de téléphone doit être inférieur à 45 caractères";
+            }
+
+            if(empty($_POST['address'])){
+                $errors['address'] = "L'adresse est obligatoire";
+            } elseif(strlen($_POST['address'])>45){
+                $errors['address'] = "L'adresse doit être inférieure à 45 caractères";
+            }
+            
+            if(empty($_POST['city'])){
+                $errors['city'] = "La ville est obligatoire";
+            } elseif(strlen($_POST['city'])>45){
+                $errors['address'] = "Le nom de la ville doit être inférieure à 45 caractères";
+            }
+            
+            if(empty($_POST['postal_code'])){
+                $errors['city'] = "Le code postal est obligatoire";
+            } elseif(strlen($_POST['postal_code']) > 45){
+                $errors['address'] = "Le code postal doit avoir 5 chiffres";
+            }               
 
             if(empty($_POST['password'])){
                 $errors['password'] = "Le mot de passe est obligatoire";
-            }elseif(!preg_match("/^[a-zA-Z0-9_-]{6,20}$/", $_POST['password'])){
+            } elseif(!preg_match("/^[a-zA-Z0-9_-]{6,20}$/", $_POST['password'])){
                 $errors['password'] = "Le mot de passe doit faire entre 6 et 20 caractères et ne doit contenir que des lettres, des chiffres ou des caractères _ et -";
             }
 
             if(empty($_POST['password_confirm'])){
                 $errors['password_confirm'] = "La confirmation du mot de passe est obligatoire";
-            }elseif($_POST['password'] != $_POST['password_confirm']){
+            } elseif($_POST['password'] != $_POST['password_confirm']){
                 $errors['password_confirm'] = "La confirmation du mot de passe n'est pas identique au mot de passe.";
             }
             
@@ -54,7 +88,7 @@ class CollectorController extends ControllerAbstract{
                 $this->app['collector.repository']->save($collector);
                 
                 return $this->redirectRoute('homepage');
-            }else{
+            } else{
                 $message = '<strong>Le formulaire contient des erreurs</strong>';
                 $message .= '<br>'.implode('<br>', $errors);
                 $this->addFlashMessage($message, 'error');
