8fdcc71f5acfca286b4fcfbaaea5bba39f5dd693
simonezg <simonezheng@gmail.com>
2017-09-13 17:06:44 +0200

    modifs ClientController

--- dev/src/Controller/ClientController.php
+++ dev/src/Controller/ClientController.php
@@ -2,7 +2,7 @@
 
 namespace Controller;
 
-use Entity\User;
+use Entity\Client;
 
 class ClientController extends ControllerAbstract{
     public function registerAction() {
@@ -15,20 +15,36 @@ class ClientController extends ControllerAbstract{
             $client
                 ->setLastname($_POST['lastname'])
                 ->setFirstname($_POST['firstname'])
-                ->setEmail($_POST['email']);
+                ->setEmail($_POST['email'])
+                ->setStartDateContract($_POST['start_date_contract'])
+                ->setPhoneNumber($_POST['phone_number'])
+                ->setBillingAddress($_POST['billing_adress'])
+                ->setPostalCode($_POST['postal_code'])
+                ->setCity($_POST['city'])
+                ->setCountry($_POST['country'])
+                ->setIsActive($_POST['is_active'])
+                ->setCompany($_POST['company'])
+                ->setSiret($_POST['siret']);
+
 
             if(empty($_POST['lastname'])){
                 $errors['lastname'] = "Le nom est obligatoire";
-            }elseif(strlen($_POST['lastname'])>100){
-                $errors['lastname'] = "Le nom doit Ãªtre infÃ©rieur Ã  100 caractÃ¨res";
+            }elseif(strlen($_POST['lastname'])>45){
+                $errors['lastname'] = "Le nom doit Ãªtre infÃ©rieur Ã  45 caractÃ¨res";
             }
 
             if(empty($_POST['firstname'])){
                 $errors['firstname'] = "Le prÃ©nom est obligatoire";
-            }elseif(strlen($_POST['firstname'])>100){
-                $errors['firstname'] = "Le prÃ©nom doit Ãªtre infÃ©rieur Ã  100 caractÃ¨res";
+            }elseif(strlen($_POST['firstname'])>45){
+                $errors['firstname'] = "Le prÃ©nom doit Ãªtre infÃ©rieur Ã  45 caractÃ¨res";
             }
 
+            if(empty($_POST['phone_number'])){
+                $errors['phone_number'] = "Le numéro de téléphone est obligatoire";
+            }elseif(!is_integer($_POST['phone_number'])){
+                $errors['phone_number'] = "Le numéro de téléphone doit être un nombre";
+            }
+            
             if(empty($_POST['email'])){
                 $errors['email'] = "L'email est obligatoire";
             }elseif(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
@@ -47,6 +63,46 @@ class ClientController extends ControllerAbstract{
                 $errors['password_confirm'] = "La confirmation du mot de passe est obligatoire";
             }elseif($_POST['password'] != $_POST['password_confirm']){
                 $errors['password_confirm'] = "La confirmation du mot de passe n'est pas identique au mot de passe.";
+            }
+            
+            if(empty($_POST['start_date_contract'])){
+                $errors['start_date_contract'] = "La date de début de contrat est obligatoire";
+            }
+            
+            if(empty($_POST['billing_address'])){
+                $errors['billing_address'] = "L'adresse de facturation est obligatoire";
+            }elseif(strlen($_POST['billing_address'])>45){
+                $errors['billing_address'] = "L'adresse doit être inférieur à 45 caractères";
+            }
+            
+            if(empty($_POST['postal_code'])){
+                $errors['postal_code'] = "Le code postal est obligatoire";
+            }elseif(strlen($_POST['postal_code'])>45){
+                $errors['postal_code'] = "Le code postal doit être inférieur à 45 caractères";
+            }
+            
+            if(empty($_POST['city'])){
+                $errors['city'] = "La ville est obligatoire";
+            }elseif(strlen($_POST['city'])>45){
+                $errors['city'] = "La ville doit être inférieure à 45 caractères";
+            }
+            
+            if(empty($_POST['country'])){
+                $errors['country'] = "Le pays est obligatoire";
+            }elseif(strlen($_POST['country'])>45){
+                $errors['country'] = "Le pays doit être inférieur à 45 caractères";
+            }
+                   
+            if(empty($_POST['company'])){
+                $errors['company'] = "L'entreprise est obligatoire";
+            }elseif(strlen($_POST['company'])>45){
+                $errors['company'] = "L'entreprise doit être inférieur à 45 caractères";
+            }
+            
+            if(empty($_POST['siret'])){
+                $errors['siret'] = "Le siret est obligatoire";
+            }elseif(strlen($_POST['siret'])>45){
+                $errors['siret'] = "Le siret doit être inférieur à 45 caractères";
             }
             
             if(empty($errors)){
