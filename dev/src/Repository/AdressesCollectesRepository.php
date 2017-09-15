33f4feedf652f2f145d35fcace937ad6ac3134f1
rekolarec <rekorec@yahoo.fr>
2017-09-15 10:31:24 +0200

    ++

--- dev/src/Repository/AdressesCollectesRepository.php
+++ dev/src/Repository/AdressesCollectesRepository.php
@@ -27,7 +27,15 @@ class AdressesCollectesRepository extends RepositoryAbstract{
                 $address 
                      ->setId_collection_address($data['id_collection_address'])
                      ->setAdress_collection($data['adress_collection'])
-                        ;
+//                     ->setPostal_code($data['postal_code'])
+//                     ->setCity($data['city'])
+//                     ->setFurther_information($data['further_information'])
+//                     ->setCountry($data['country'])
+//                     ->setCollection_day($data['collection_day'])
+//                     ->setClient_idclient($data['client_idclient'])
+//                     ->setLocation_processing_idlocation_processing($data['location_processing_idlocation_processing'])
+//                     ->setFirm_type($data['firm_type'])
+                    ;
                 
                         return $address;
     }
